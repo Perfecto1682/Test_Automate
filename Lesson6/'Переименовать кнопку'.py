8180e9839320b403b2911ab2a00319e907e2c2eb
Perfecto1682 <perfecto_honrado5@mail.ru>
2024-11-02 23:33:35 +0300

    New HW

---
+++ Lesson6/'Переименовать кнопку'.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+# Путь к chromedriver
+service = Service(executable_path=r'C:\Chromedriver\chromedriver.exe')
+
+# Запуск веб-драйвера
+driver = webdriver.Chrome(service=service)
+
+# Открыть сайт
+driver.get("http://uitestingplayground.com/textinput")
+
+# Написать в поисковую строку текст SkyPro
+input_field = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, '#newButtonName')))
+input_field.send_keys("SkyPro")
+
+# Нажать на кнопку
+search_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, '#updatingButton')))
+search_button.click()
+
+# Проверяем, что кнопка переименовалась
+try:
+    WebDriverWait(driver, 10).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#updatingButton'), "SkyPro"))
+    print("Кнопка была переименована успешно.")
+except TimeoutException:
+    print("Кнопка не была переименована.")
+
+
+# Закрыть веб-драйвер
+driver.quit()
