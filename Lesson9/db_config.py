e36cecaeca6c25c90adec392544946a279b460ad
Perfecto1682 <perfecto_honrado5@mail.ru>
2024-11-29 14:29:21 +0300

    Update

--- Lesson9/db_config.py
+++ Lesson9/db_config.py
@@ -6,12 +6,18 @@ from sqlalchemy.orm import sessionmaker
 # Загружаем переменные из файла .env
 load_dotenv()
 
-# Получаем URL базы данных
+# Получаем URL базы данных из переменных окружения
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Проверяем, что DATABASE_URL установлен, иначе выбрасываем ошибку
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in the environment variables")
 
-# Создаем движок и сессию
+# Создаем SQLAlchemy движок для подключения к базе данных
 engine = create_engine(DATABASE_URL)
+
+# Создаем фабрику сессий, привязанную к движку
 Session = sessionmaker(bind=engine)
+
+# Инициализируем сессию для взаимодействия с базой данных
 session = Session()
