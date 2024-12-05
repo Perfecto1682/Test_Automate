9bf5c90166d557b486da46c50e8ef9d60abaaf1d
Perfecto1682 <perfecto_honrado5@mail.ru>
2024-12-05 20:54:16 +0300

    Update

--- Lesson10/pages/shop_page_L10.py
+++ Lesson10/pages/shop_page_L10.py
@@ -1,78 +1,49 @@
+import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class ShopPage:
-    """
-    Класс для работы с магазином.
-
-    :param driver: WebDriver instance для управления браузером.
-    """
-
     def __init__(self, driver):
         self.driver = driver
 
+    @allure.step('Открытие страницы магазина по URL {url}')
     def open(self, url: str) -> None:
-        """
-        Открывает указанную страницу.
-
-        :param url: URL страницы.
-        """
         self.driver.get(url)
 
+    @allure.step('Авторизация с именем пользователя {username} и паролем {password}')
     def login(self, username: str, password: str) -> None:
-        """
-        Выполняет авторизацию.
-
-        :param username: Имя пользователя.
-        :param password: Пароль пользователя.
-        """
         self.driver.find_element(By.ID, "user-name").send_keys(username)
         self.driver.find_element(By.ID, "password").send_keys(password)
         self.driver.find_element(By.ID, "login-button").click()
 
+    @allure.step('Добавление товаров в корзину')
     def add_items_to_cart(self) -> None:
-        """
-        Добавляет товары в корзину.
-        """
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//button[@data-test='add-to-cart-sauce-labs-backpack']"))
         ).click()
         self.driver.find_element(By.XPATH, "//button[@data-test='add-to-cart-sauce-labs-bolt-t-shirt']").click()
         self.driver.find_element(By.XPATH, "//button[@data-test='add-to-cart-sauce-labs-onesie']").click()
 
+    @allure.step('Переход в корзину')
     def go_to_cart(self) -> None:
-        """
-        Переходит в корзину.
-        """
         self.driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
 
+    @allure.step('Оформление заказа')
     def checkout(self) -> None:
-        """
-        Нажимает кнопку Checkout для оформления заказа.
-        """
         self.driver.find_element(By.ID, "checkout").click()
 
+    @allure.step('Заполнение формы оформления заказа с именем {first_name},'
+                 ' фамилией {last_name}, почтовым индексом {postal_code}')
     def fill_checkout_form(self, first_name: str, last_name: str, postal_code: str) -> None:
-        """
-        Заполняет форму оформления заказа.
-
-        :param first_name: Имя пользователя.
-        :param last_name: Фамилия пользователя.
-        :param postal_code: Почтовый индекс.
-        """
         self.driver.find_element(By.ID, "first-name").send_keys(first_name)
         self.driver.find_element(By.ID, "last-name").send_keys(last_name)
         self.driver.find_element(By.ID, "postal-code").send_keys(postal_code)
         self.driver.find_element(By.ID, "continue").click()
 
+    @allure.step('Получение итоговой суммы заказа')
     def get_total_price(self) -> str:
-        """
-        Получает итоговую сумму заказа.
-
-        :return: Итоговая сумма в виде строки.
-        """
         total_element = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, "summary_total_label"))
         )
